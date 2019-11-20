89cbf165d62ad0f174bcc77ae6761fd4296ce508
Aiman Almureish <aiman.almureish@gmail.com>
2019-11-20 19:44:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your functions here
+function map(array, newFunc){
+    let arr = [];
+      console.log(arr)
+      array.forEach(element => {
+          arr.push(newFunc(element))
+      });
+      return arr
+    }
+    console.log(arr)
+
+
+
+
+    function reduce(array, newFunc,startVal){
+        let newVal;
+        newVal = Boolean(newVal)
+
+
+
+    //     for(let i=0; i< array.length; i++){  
+    //         startVal = startVal + newFunc(array[i])
+    //  }
+
+    
+
+        for(let i=0; i< array.length; i++){  
+            startVal = newFunc(array[i],newVal)
+     }
+     return startVal
+    }
+  
+  
